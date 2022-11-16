60d80a737e15271312b4d8ba4859d3fc2d88d822
Daniel-Riv <daniel.rivas@uptc.edu.co>
2022-11-15 21:16:11 -0500

    error floop

--- src/components/Start/Nav/Items.js
+++ src/components/Start/Nav/Items.js
@@ -1,28 +1,16 @@
 import React, { useState } from 'react';
 import {
-  Box,
-  Divider,
-  Drawer,
   Grid,
-  List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Toolbar,
-  Typography,
 } from '@mui/material';
 import BookIcon from '@mui/icons-material/Book';
 import DeleteIcon from '@mui/icons-material/Delete';
 import UpgradeIcon from '@mui/icons-material/Upgrade';
 
 export const Items = ({ id, name }) => {
-  const [addMmodal, setAddModal] = useState(false);
-
-  const handleOpenModal = () => {
-    setAddModal(true);
-  };
-  const handleCloseModal = () => setAddModal(false);
 
   const deleteMatter = async id => {
     const response = await fetch(
@@ -47,7 +35,7 @@ export const Items = ({ id, name }) => {
           <Grid container>
             <ListItemText primary={name} />
             <DeleteIcon onClick={() => deleteMatter(id)} />
-            <UpgradeIcon  onClick={handleOpenModal()}/>
+            <UpgradeIcon  />
             {/* <ListItemText secondary={'Lorem insert line Daniel RIVAS 123'}/> */}
           </Grid>
         </ListItemButton>
